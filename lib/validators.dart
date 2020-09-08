072b3dff914f6915d4862b95408a0a0c094ca503
FlutterDev <Keicy6778@gmail.com>
2020-09-08 23:31:52 +0100

    2020-09-08

--- lib/validators.dart
+++ lib/validators.dart
@@ -1,4 +1,8 @@
 class KeicyValidators {
+  static final RegExp nameRegExp = RegExp(
+    r'^[a-zA-Z]{1}.+$',
+  );
+
   static final RegExp emailRegExp = RegExp(
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
   );
@@ -11,13 +15,17 @@ class KeicyValidators {
     r'([/|.|\w|\s|-])*\.(?:jpg|gif|png)',
   );
 
-  static final RegExp simpleTextRegExp = RegExp(
-    r'^[a-zA-Z]{1}.+$',
-  );
-
   static final RegExp firebaseStorageFileRegExp = RegExp(
     r'(.appspot.com\/[a-z]\/)|(\?)',
   );
+
+  static final RegExp storageFileName = RegExp(
+    r'(%2)|([?])',
+  );
+
+  static bool isValidateName(String string) {
+    return nameRegExp.hasMatch(string);
+  }
 
   static bool isValidEmail(String email) {
     return emailRegExp.hasMatch(email);
@@ -27,19 +35,11 @@ class KeicyValidators {
     return imageUrlRegExp.hasMatch(imageUrl);
   }
 
-  static bool isEmpty(String string) {
-    return (string == '' || string == null) ? false : true;
-  }
-
-  static bool isSimpleText(String string) {
-    return simpleTextRegExp.hasMatch(string);
-  }
-
   static bool isValidImageName(String imageName) {
     return imageNameRegExp.hasMatch(imageName);
   }
 
-  static final RegExp storageFileName = RegExp(
-    r'(%2)|([?])',
-  );
+  static bool isEmpty(String string) {
+    return (string == '' || string == null) ? false : true;
+  }
 }
