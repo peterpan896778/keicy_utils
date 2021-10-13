1cf4328acf3d31686bfc5a85af3bdf91c6b59f98
iscojin <flutterdev9715@gmail.com>
2021-10-13 10:59:47 +0100

    2021-10-13

--- lib/date_time_convert.dart
+++ lib/date_time_convert.dart
@@ -4,89 +4,95 @@ import 'package:flutter/material.dart';
 class KeicyDateTime {
   static const String initFormat = "Y-m-d";
 
-  static DateTime convertDateStringToDateTime({@required String dateString, datePattern = '-', timePattern = ":"}) {
+  static DateTime? convertDateStringToDateTime({@required String? dateString, datePattern = '-', timePattern = ":", @required bool? isUTC}) {
     try {
       if (dateString == "" || dateString == null) return null;
-
       List<String> listDateTime = dateString.split(" ");
       List<String> listDate = listDateTime[0].split(datePattern);
-      List<String> listTime = (listDateTime.length == 2) ? listDateTime[1].split(timePattern) : null;
+      List<String>? listTime = (listDateTime.length == 2) ? listDateTime[1].split(timePattern) : null;
 
-      return DateTime(
-        int.parse((listDate != null && listDate.length >= 1 && listDate[0] != "") ? listDate[0].toString() : "0"),
-        int.parse((listDate != null && listDate.length >= 2 && listDate[1] != "") ? listDate[1].toString() : "0"),
-        int.parse((listDate != null && listDate.length >= 3 && listDate[2] != "") ? listDate[2].toString() : "0"),
-        int.parse((listTime != null && listTime.length >= 1 && listTime[0] != "") ? listTime[0].toString() : "0"),
-        int.parse((listTime != null && listTime.length >= 2 && listTime[1] != "") ? listTime[1].toString() : "0"),
-        int.parse((listTime != null && listTime.length >= 3 && listTime[2] != "") ? listTime[2].toString() : "0"),
+      DateTime dt = DateTime(
+        int.parse((listDate.length >= 1 && listDate[0] != "") ? listDate[0].toString() : "0"),
+        int.parse((listDate.length >= 2 && listDate[1] != "") ? listDate[1].toString() : "0"),
+        int.parse((listDate.length >= 3 && listDate[2] != "") ? listDate[2].toString() : "0"),
+        int.parse((listTime!.length >= 1 && listTime[0] != "") ? listTime[0].toString() : "0"),
+        int.parse((listTime.length >= 2 && listTime[1] != "") ? listTime[1].toString() : "0"),
+        int.parse((listTime.length >= 3 && listTime[2] != "") ? listTime[2].toString() : "0"),
       );
+
+      if (isUTC!)
+        return dt.toUtc();
+      else
+        return dt.toLocal();
     } catch (e) {
       return null;
     }
   }
 
-  static int convertDateStringToMilliseconds({@required String dateString, datePattern = '-', timePattern = ":"}) {
+  static int? convertDateStringToMilliseconds({@required String? dateString, datePattern = '-', timePattern = ":", @required bool? isUTC}) {
     try {
       if (dateString == null || dateString == "") return null;
       return convertDateStringToDateTime(
         dateString: dateString,
         datePattern: datePattern,
         timePattern: timePattern,
-      ).millisecondsSinceEpoch;
+        isUTC: isUTC,
+      )!
+          .millisecondsSinceEpoch;
     } catch (e) {
       return null;
     }
   }
 
-  static String convertDateTimeToDateString({@required DateTime dateTime, String formats = initFormat}) {
+  static String convertDateTimeToDateString({@required DateTime? dateTime, String formats = initFormat, @required bool? isUTC}) {
     try {
       if (dateTime == null) return "";
-      return DateTimeFormat.format(dateTime, format: formats);
+      if (isUTC!)
+        return DateTimeFormat.format(dateTime.toUtc(), format: formats);
+      else
+        return DateTimeFormat.format(dateTime.toLocal(), format: formats);
     } catch (e) {
       return "";
     }
   }
 
-  static String convertMillisecondsToDateString({@required int ms, String formats = initFormat}) {
+  static String convertMillisecondsToDateString({@required int? ms, String formats = initFormat, @required bool? isUTC}) {
     try {
       if (ms == null) return "";
-      return convertDateTimeToDateString(dateTime: DateTime.fromMillisecondsSinceEpoch(ms), formats: formats);
+      return convertDateTimeToDateString(
+        dateTime: DateTime.fromMillisecondsSinceEpoch(ms),
+        formats: formats,
+        isUTC: isUTC,
+      );
     } catch (e) {
       return "";
     }
   }
 
-  static List<DateTime> getDateTimesForWeek({DateTime dateTime}) {
+  static List<DateTime> getDateTimesForWeek({DateTime? dateTime}) {
     if (dateTime == null) dateTime = DateTime.now();
     switch (dateTime.weekday) {
       case 1:
         return [dateTime, dateTime.add(Duration(days: 6))];
-        break;
       case 2:
         return [dateTime.subtract(Duration(days: 1)), dateTime.add(Duration(days: 5))];
-        break;
       case 3:
         return [dateTime.subtract(Duration(days: 2)), dateTime.add(Duration(days: 4))];
-        break;
       case 4:
         return [dateTime.subtract(Duration(days: 3)), dateTime.add(Duration(days: 3))];
-        break;
       case 5:
         return [dateTime.subtract(Duration(days: 4)), dateTime.add(Duration(days: 2))];
-        break;
       case 6:
         return [dateTime.subtract(Duration(days: 5)), dateTime.add(Duration(days: 1))];
-        break;
       case 7:
         return [dateTime.subtract(Duration(days: 6)), dateTime];
-        break;
       default:
     }
 
     return [dateTime, dateTime];
   }
 
-  static List<DateTime> getDateTimesForMonth({DateTime dateTime}) {
+  static List<DateTime> getDateTimesForMonth({DateTime? dateTime}) {
     if (dateTime == null) dateTime = DateTime.now();
 
     return [
--- lib/validators.dart
+++ lib/validators.dart
@@ -39,7 +39,23 @@ class KeicyValidators {
     return imageNameRegExp.hasMatch(imageName);
   }
 
-  static bool isEmpty(String string) {
+  static bool isEmpty(String? string) {
     return (string == '' || string == null) ? false : true;
+  }
+
+  static String? getYoutubeVideoId(String url, {bool trimWhitespaces = true}) {
+    if (!url.contains("http") && (url.length == 11)) return url;
+    if (trimWhitespaces) url = url.trim();
+
+    for (var exp in [
+      RegExp(r"^https:\/\/(?:www\.|m\.)?youtube\.com\/watch\?v=([_\-a-zA-Z0-9]{11}).*$"),
+      RegExp(r"^https:\/\/(?:www\.|m\.)?youtube(?:-nocookie)?\.com\/embed\/([_\-a-zA-Z0-9]{11}).*$"),
+      RegExp(r"^https:\/\/youtu\.be\/([_\-a-zA-Z0-9]{11}).*$")
+    ]) {
+      Match? match = exp.firstMatch(url);
+      if (match != null && match.groupCount >= 1) return match.group(1);
+    }
+
+    return null;
   }
 }
